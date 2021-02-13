6c494294c4161347950b20261800f3dce75935a7
Ashir <ashir.rao26@gmail.com>
2021-02-13 14:09:14 -0800

    snipe test

--- lamb.py
+++ lamb.py
@@ -1,3 +1,6 @@
 #instead of cow, i use lamb
 
-print("ahahahah")
+print("sniped?")
+print("snake gang")
+truth = lambda x: print("sheep is better than cow in yo face Farmer John")
+print("FJ uses child labor")
