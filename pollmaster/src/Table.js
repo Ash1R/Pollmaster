39828a82f6a7c7f752155b6d7be8f58828b645e1
Nasir Khan <rnakhan@gmail.com>
2021-02-13 17:29:22 -0800

    some changes to base react project by nasir

---
+++ pollmaster/src/Table.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class Table extends Component {
+  render() {
+    return (
+    <table>
+      <tr>
+        <td>Name</td>
+        <td>Grade</td>
+      </tr>
+      <tr>
+        <td>Ashir</td>
+        <td>9th</td>
+      </tr>
+      <tr>
+        <td>Neena</td>
+        <td>Masters</td>
+      </tr>
+    </table>
+    );
+  }
+}
+
+export default Table
